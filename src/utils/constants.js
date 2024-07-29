d3f49f8133ef9bfdc53bbec9e143d0f934c5b259
Kudoo39 <thpthv.nguyenphuduykhang@gmail.com>
2024-07-29 18:50:03 +0300

    Add a new whitelist domain

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,7 @@
 // Some domains are allowed to access server resources
-export const WHITELIST_DOMAINS = []
+export const WHITELIST_DOMAINS = [
+  'https://trelloproject.vercel.app'
+]
 
 export const BOARD_TYPES = {
   PUBLIC: 'public',
